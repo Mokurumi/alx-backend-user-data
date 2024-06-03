0444b30b6f24ac69b473f6a9ef72b20fa36917de
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-04 02:08:12 +0300

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,11 @@ def unauthorized() -> str:
     Raise a 401 error by using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Raise a 403 error by using abort
+    """
+    abort(403)
