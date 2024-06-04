df8f77d73e7d71e0c405cc7f679a2cc7ee5f9880
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-04 03:01:26 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 """
 BasicAuth class
+
+Add the method def decode_base64_authorization_header(self, base64_authorization_header: str) -> str: in the class BasicAuth that returns the decoded value of a Base64 string base64_authorization_header:
+
+Return None if base64_authorization_header is None
+Return None if base64_authorization_header is not a string
+Return None if base64_authorization_header is not a valid Base64 - you can use try/except
+Otherwise, return the decoded value as UTF8 string - you can use decode('utf-8')
 """
 
 
@@ -22,3 +29,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+          """ decode_base64_authorization_header method
+          """
+          if base64_authorization_header is None:
+                return None
+          if type(base64_authorization_header) is not str:
+                return None
+          try:
+                return base64_authorization_header.decode('utf-8')
+          except Exception:
+                return None
