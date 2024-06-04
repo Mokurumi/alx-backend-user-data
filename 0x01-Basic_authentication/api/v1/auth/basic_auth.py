e7bd9564840c4383515817573c82462e92828d73
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-04 03:16:00 +0300

    fixx

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 """
 BasicAuth class
+Add the method def decode_base64_authorization_header(self, base64_authorization_header: str) -> str: in the class BasicAuth that returns the decoded value of a Base64 string base64_authorization_header:
+
+Return None if base64_authorization_header is None
+Return None if base64_authorization_header is not a string
+Return None if base64_authorization_header is not a valid Base64 - you can use try/except
+Otherwise, return the decoded value as UTF8 string - you can use decode('utf-8')
 """
 
 
@@ -32,15 +38,14 @@ class BasicAuth(Auth):
                                            ) -> str:
           """ decode_base64_authorization_header method
           """
-          if base64_authorization_header is None:
+            if base64_authorization_header is None:
                 return None
-          if not isinstance(base64_authorization_header, str):
+            if type(base64_authorization_header) is not str:
                 return None
-          try:
-                const decoded = base64.b64decode(
-                    base64_authorization_header,
-                    validate=True
-                )
-                return decoded.decode('utf-8')
-          except (binascii.Error, UnicodeDecodeError):
+            try:
+                base64_bytes = base64_authorization_header.encode('utf-8')
+                message_bytes = base64.b64decode(base64_bytes)
+                message = message_bytes.decode('utf-8')
+                return message
+            except binascii.Error:
                 return None
