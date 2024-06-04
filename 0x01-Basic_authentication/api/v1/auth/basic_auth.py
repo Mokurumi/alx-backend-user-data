aed633b119206e735817c110b56a837d1baa9245
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-04 03:34:52 +0300

    remove base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python3
 """
 BasicAuth class
-
-Add the method def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'): in the class BasicAuth that returns the User instance based on his email and password.
-
-Return None if user_email is None or not a string
-Return None if user_pwd is None or not a string
-Return None if your database (file) doesn’t contain any User instance with email equal to user_email - you should use the class method search of the User to lookup the list of users based on their email. Don’t forget to test all cases: “what if there is no user in DB?”, etc.
-Return None if user_pwd is not the password of the User instance found - you must use the method is_valid_password of User
-Otherwise, return the User instance
 """
 
 
 from api.v1.auth.auth import Auth
-from typing import TypeVar
 from models.user import User
-import base64
-import binascii
 
 
 class BasicAuth(Auth):
@@ -34,24 +23,6 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
-
-    def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str
-                                           ) -> str:
-          """ decode_base64_authorization_header method
-          """
-            # if base64_authorization_header is None:
-            #     return None
-            # if type(base64_authorization_header) is not str:
-            #     return None
-            try:
-                str_bytes = base64.b64decode(
-                    base64_authorization_header,
-                    validate=True
-                )
-                return str_bytes.decode('utf-8')
-        except Exception:
-                return None
 
     def extract_user_credentials(self, decoded_base64_authorization_header: str
                                  ) -> (str, str):
