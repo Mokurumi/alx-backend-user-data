56e363a9dcb62c392c165eb7b4d166862b183d32
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-09 12:34:58 +0300

    Log Formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
-"""
-Filtering module
-"""
+'''
+This module contains the function filter_datum
+'''
+
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ Returns the log message obfuscated """
-    return re.sub(r'(\b(?:' + '|'.join(fields) + r')\b)=\S+',
-                  lambda x: x.group(1) + '=' + redaction,
-                  message)
+    for field in fields:
+        message = re.sub(rf'{field}=.*?{separator}', f'{field}={redaction}{separator}', message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Constructor
+        """
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format method
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
