e01c71507ecb1fd2901e83777332bfb19de1e296
Alpha Mokurumi <alpha@nathandigital.com>
2024-06-10 10:29:01 +0300

    get_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 '''
 This module contains the function filter_datum
+
+Use user_data.csv for this task
+
+Implement a get_logger function that takes no arguments and returns a logging.Logger object.
+
+The logger should be named "user_data" and only log up to logging.INFO level. It should not propagate messages to other loggers. It should have a StreamHandler with RedactingFormatter as formatter.
+
+Create a tuple PII_FIELDS constant at the root of the module containing the fields from user_data.csv that are considered PII. PII_FIELDS can contain only 5 fields - choose the right list of fields that can are considered as “important” PIIs or information that you must hide in your logs. Use it to parameterize the formatter.
+
+
 '''
 
 
 import re
 from typing import List
 import logging
+import os
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -40,3 +54,16 @@ class RedactingFormatter(logging.Formatter):
         """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Returns a logging.Logger object
+    """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    ch = logging.StreamHandler()
+    ch.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(ch)
+    return logger
