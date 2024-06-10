2e35e66759b58d8d6156960e79120a7f848df870
Alpha Mokurumi <alpha@nathandigital.com>
2024-06-10 16:39:10 +0300

    encrypt password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+encrypt_password.py
+'''
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    '''
+    Returns a salted, hashed password, which is a byte string.
+    '''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
