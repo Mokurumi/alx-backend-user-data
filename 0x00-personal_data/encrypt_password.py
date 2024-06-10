596bedc98ec93b3b2e45b29757486005bdfeb0de
Alpha Mokurumi <alpha@nathandigital.com>
2024-06-10 16:41:13 +0300

    is valid pwd

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,10 @@ def hash_password(password: str) -> bytes:
     Returns a salted, hashed password, which is a byte string.
     '''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''
+    Returns a boolean.
+    '''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
