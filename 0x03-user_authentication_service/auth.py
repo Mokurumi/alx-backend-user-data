d71ad1431decf4ebf9662aaedc4e05f008ebc7ed
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-14 01:59:27 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,3 +31,15 @@ class Auth:
         except ValueError:
             pass
         return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except ValueError:
+            return False
+        except Exception:
+            return False
