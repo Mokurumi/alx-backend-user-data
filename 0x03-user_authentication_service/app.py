8f32f590a62e922e1c5902685abf77066f188cf9
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-14 01:49:11 +0300

    register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,9 +5,12 @@ Main module
 
 
 from flask import Flask, jsonify
+from flask import request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
@@ -15,6 +18,19 @@ def home() -> str:
     """Home endpoint
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """Users endpoint
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
