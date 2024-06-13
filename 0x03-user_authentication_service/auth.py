8e8b11fa680c2c4307a151ca93bfac7165deab12
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-14 02:04:20 +0300

    Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,3 +50,11 @@ class Auth:
             return False
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session
+        """
+        user = self._db.find_user_by(email=email)
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
