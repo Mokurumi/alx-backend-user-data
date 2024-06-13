f2e6473c8abb939c9c392789e16a6daaba334915
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-14 01:18:58 +0300

    encrypt pwd

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
